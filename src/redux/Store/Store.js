19a14d23729abbdf9ea671aa69f98900ebafa618
barshatpradhan <barshatpradhan321@gmail.com>
2023-11-05 13:09:40 +0530

    few changes

--- src/redux/Store/Store.js
+++ src/redux/Store/Store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import CartSlice from "../Slice/CartSlice";
-import AuthSlice from "../Slice/AuthSlice";
+import AuthSlice from "../Slice/AuthSlice"
 
 export const store = configureStore({
   reducer: {
